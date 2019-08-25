f294cbb629d3bf8c1201e9ab39c39caac1926285
kheyfetsdan <kheyfets94@yandex.ru>
2019-08-25 19:14:47 +0300

    Start project

--- app/src/androidTest/java/com/example/pettestproject/kakaoTest/SecondTest.kt
+++ app/src/androidTest/java/com/example/pettestproject/kakaoTest/SecondTest.kt
@@ -1,16 +1,13 @@
 package com.example.pettestproject.kakaoTest
 
-
+import android.support.test.rule.ActivityTestRule
+import com.agoda.kakao.screen.Screen
 import com.agoda.kakao.screen.Screen.Companion.idle
 import com.example.pettestproject.MainActivity
 import org.junit.Rule
-import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-class TestMainActivity {
+class SecondTest {
 
     @Rule
     @JvmField
@@ -20,11 +17,17 @@ class TestMainActivity {
     private val mainActivity = mainActivityScreen()
 
     @Test
-    fun mainTest() {
+    fun mainTestFirst() {
         mainActivity.lable.hasText("Hello Test!")
-        mainActivity.buttonText.click()
-        mainActivity.lable.hasText("Change your *** pussy")
         mainActivity.buttonClear.click()
         mainActivity.lable.hasText("")
+    }
+
+    @Test
+    fun mainTestSecond() {
+        mainActivity.lable.hasText("Hello Test!")
+        idle(5000L)
+        mainActivity.buttonText.click()
+        mainActivity.lable.hasText("Change your *** pussy")
     }
 }
