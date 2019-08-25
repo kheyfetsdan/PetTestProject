b59cad0f284e0990fc1bf645c06b800645cd8de7
kheyfetsdan <kheyfets94@yandex.ru>
2019-08-25 13:19:29 +0300

    Start project

---
+++ app/src/main/java/com/example/pettestproject/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.pettestproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun changeText (view: View) {
+
+        val showTextView = findViewById<TextView>(R.id.labelText)
+
+        showTextView.text = "Change your fucking text"
+    }
+
+    fun clear (view: View) {
+
+        val showTextView = findViewById<TextView>(R.id.labelText)
+
+        showTextView.text = ""
+    }
+}
